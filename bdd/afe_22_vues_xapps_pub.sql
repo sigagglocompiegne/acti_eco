085058cfa060a1e52a7433f1b206b9d388581241
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-02-17 08:29:18 +0100

    Create afe_22_vues_xapps_pub.sql

---
+++ bdd/afe_22_vues_xapps_pub.sql
@@ -0,0 +1,10 @@
+/*Amt_Fon_Eco V1.0*/
+/*Creation des vues applicatives gd public stockées dans le schéma x_apps_public */
+/* afe_62_vues_xapps_pu.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+
+-- pas de vues grand public pour ce projet
