1328c98dbc2ae28c7ef2913e1c8fb15e719dcff5
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-02-17 08:29:51 +0100

    Create afe_23_vues_xopendate.sql

---
+++ bdd/afe_23_vues_xopendate.sql
@@ -0,0 +1,10 @@
+/*Amt_Fon_Eco V1.0*/
+/*Creation des vues applicatives OpenData stockées dans le schéma x_opendata */
+/* afe_23_vues_xopendata.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+
+-- pas de vues Open Data pour ce projet
