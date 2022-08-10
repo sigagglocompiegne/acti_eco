705178fd13c91e54a3e2f56a1f4a62c345b05792
gregbodet <37414914+gregbodet@users.noreply.github.com>
2022-08-10 08:44:41 +0200

    Update afe_101_squelette.sql

--- bdd/afe_101_squelette.sql
+++ bdd/afe_101_squelette.sql
@@ -6,6 +6,17 @@
 /* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
 /* Auteur : Grégory Bodet */
 
+-- NETTOYAGE
+
+/* TABLE */
+DROP TABLE  m_activite_eco.an_eco_pole;
+
+/* LISTE DE VALEUR */
+DROP TABLE m_activite_eco.lt_eco_dest;
+
+
+
+
 -- ####################################################################################################################################################
 -- ###                                                                                                                                              ###
 -- ###                                                                SCHEMA                                                                        ###
@@ -52,13 +63,46 @@ GRANT INSERT, SELECT, UPDATE, DELETE ON TABLES TO sig_edit;
 -- ###                                                                                                                                              ###
 -- ####################################################################################################################################################
 
--- ################################################################# Domaine valeur - [nom_table]  ###############################################
+-- ################################################################# Domaine valeur - [lt_eco_dest]  ###############################################
+
+-- Table: m_activite_eco.lt_eco_dest
+
+-- DROP TABLE m_activite_eco.lt_eco_dest;
+
+CREATE TABLE m_activite_eco.lt_eco_dest
+(
+    code character varying(2) COLLATE pg_catalog."default" NOT NULL,
+    valeur character varying(30) COLLATE pg_catalog."default",
+    CONSTRAINT lt_eco_dest_pkkey PRIMARY KEY (code)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE m_activite_eco.lt_eco_dest
+    OWNER to create_sig;
+
+GRANT ALL ON TABLE m_activite_eco.lt_eco_dest TO sig_create;
+
+GRANT SELECT ON TABLE m_activite_eco.lt_eco_dest TO sig_read;
+
+GRANT ALL ON TABLE m_activite_eco.lt_eco_dest TO create_sig;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE m_activite_eco.lt_eco_dest TO sig_edit;
+
+COMMENT ON TABLE m_activite_eco.lt_eco_dest
+    IS 'Liste de valeurs de destination (table pôle et site)';
+
+COMMENT ON COLUMN m_activite_eco.lt_eco_dest.code
+    IS 'Code de la destination principale du site ou du Pôle';
+
+COMMENT ON COLUMN m_activite_eco.lt_eco_dest.valeur
+    IS 'Libellé de la destination principale du site ou du Pôle';
 
 
 
--- ################################################################# Domaine valeur - destination  ###############################################
 
--- cette liste de valeur est inclue dans le projet document d'urbanisme (lt_destdomi)
 
 -- ####################################################################################################################################################
 -- ###                                                                                                                                              ###
@@ -67,7 +111,72 @@ GRANT INSERT, SELECT, UPDATE, DELETE ON TABLES TO sig_edit;
 -- ####################################################################################################################################################
 
 
--- ################################################# [nom_table] ##################################
+-- ################################################# [geo_eco_pole] ##################################
+
+-- Table: m_activite_eco.an_eco_pole
+
+-- DROP TABLE m_activite_eco.an_eco_pole;
+
+CREATE TABLE m_activite_eco.an_eco_pole
+(
+    idpole character varying(7) COLLATE pg_catalog."default" NOT NULL,
+    nom_pole character varying(100) COLLATE pg_catalog."default",
+    dest character varying(2) COLLATE pg_catalog."default" DEFAULT '00'::character varying,
+    op_sai character varying(80) COLLATE pg_catalog."default",
+    org_sai character varying(100) COLLATE pg_catalog."default",
+    date_sai timestamp without time zone DEFAULT now(),
+    date_maj timestamp without time zone,
+    CONSTRAINT an_eco_pole_pkey PRIMARY KEY (idpole),
+    CONSTRAINT lt_sa_dest_fkey FOREIGN KEY (dest)
+        REFERENCES m_activite_eco.lt_eco_dest (code) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE m_activite_eco.an_eco_pole
+    OWNER to create_sig;
+
+GRANT ALL ON TABLE m_activite_eco.an_eco_pole TO sig_create;
+
+GRANT SELECT ON TABLE m_activite_eco.an_eco_pole TO sig_read;
+
+GRANT ALL ON TABLE m_activite_eco.an_eco_pole TO create_sig;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE m_activite_eco.an_eco_pole TO sig_edit;
+
+COMMENT ON TABLE m_activite_eco.an_eco_pole
+    IS 'Table alphanumérique recensement les pôle d''activités économiques (regroupement de sites). Usage statistique au niveau Régional';
+
+COMMENT ON COLUMN m_activite_eco.an_eco_pole.idpole
+    IS 'Identifiant unique du pôle d''activité';
+
+COMMENT ON COLUMN m_activite_eco.an_eco_pole.nom_pole
+    IS 'Libellé du pôle d''activités';
+
+COMMENT ON COLUMN m_activite_eco.an_eco_pole.date_sai
+    IS 'Date de saisie';
+
+COMMENT ON COLUMN m_activite_eco.an_eco_pole.date_maj
+    IS 'Date de mise à jour';
+
+COMMENT ON COLUMN m_activite_eco.an_eco_pole.op_sai
+    IS 'Opérateur de saisie initial';
+
+COMMENT ON COLUMN m_activite_eco.an_eco_pole.org_sai
+    IS 'Organisme de saisie initial';
+
+COMMENT ON COLUMN m_activite_eco.an_eco_pole.dest
+    IS 'Destination du pôle (issue de la destination des sites)';
+
+
+
+
+
+
 
 
 				  
