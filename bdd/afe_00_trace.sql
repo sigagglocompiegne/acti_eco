acbacc4c8c077bcbaf30554018400133a44081c2
gregbodet <37414914+gregbodet@users.noreply.github.com>
2022-10-21 10:08:37 +0200

    Update afe_00_trace.sql

--- bdd/afe_00_trace.sql
+++ bdd/afe_00_trace.sql
@@ -23,4 +23,5 @@
 -- 2021-12-03 : GB / modification des attributs exportés des établissements
 -- 2022-01-19 : GB / modification trigger de gestion des lots à la mise à jour sur (geo_v_lot_eco), correctif sur gestion des surfaces des lots
 -- 2022-08-09 : GB / réflexion sur un nouvel modèle de données autour de l'activité économique (modèle en cours de refonte)
+-- 2022-10-21 : GB / initialisation du nouveau modèle de données
 */
